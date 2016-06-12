5650586fb46eb9bf631b19bcc7c4a001bc093680
Chen, Xi <chenxi40682@gmail.com>
2016-06-13 03:14:38 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,50 +1,59 @@
+# Load: activity labels
+activity_labels <- read.table("activity_labels.txt")[,2]
 
-## step 1
-# read all the data
-test.labels <- read.table("test/y_test.txt", col.names="label")
-test.subjects <- read.table("test/subject_test.txt", col.names="subject")
-test.data <- read.table("test/X_test.txt")
-train.labels <- read.table("train/y_train.txt", col.names="label")
-train.subjects <- read.table("train/subject_train.txt", col.names="subject")
-train.data <- read.table("train/X_train.txt")
+# Load: data column names
+features <- read.table("features.txt")[,2]
 
-# put it together in the format of: subjects, labels, everything else
-data <- rbind(cbind(test.subjects, test.labels, test.data),
-              cbind(train.subjects, train.labels, train.data))
+# Extract only the measurements on the mean and standard deviation for each measurement.
+extract_features <- grepl("mean|std", features)
 
-## step 2
-# read the features
-features <- read.table("features.txt", strip.white=TRUE, stringsAsFactors=FALSE)
-# only retain features of mean and standard deviation
-features.mean.std <- features[grep("mean\\(\\)|std\\(\\)", features$V2), ]
+# Load and process X_test & y_test data.
+X_test <- read.table("test/X_test.txt")
+y_test <- read.table("test/y_test.txt")
+subject_test <- read.table("test/subject_test.txt")
 
-# select only the means and standard deviations from data
-# increment by 2 because data has subjects and labels in the beginning
-data.mean.std <- data[, c(1, 2, features.mean.std$V1+2)]
+names(X_test) = features
 
-## step 3
-# read the labels (activities)
-labels <- read.table("activity_labels.txt", stringsAsFactors=FALSE)
-# replace labels in data with label names
-data.mean.std$label <- labels[data.mean.std$label, 2]
+# Extract only the measurements on the mean and standard deviation for each measurement.
+X_test = X_test[,extract_features]
 
-## step 4
-# first make a list of the current column names and feature names
-good.colnames <- c("subject", "label", features.mean.std$V2)
-# then tidy that list
-# by removing every non-alphabetic character and converting to lowercase
-good.colnames <- tolower(gsub("[^[:alpha:]]", "", good.colnames))
-# then use the list as column names for data
-colnames(data.mean.std) <- good.colnames
+# Load activity labels
+y_test[,2] = activity_labels[y_test[,1]]
+names(y_test) = c("Activity_ID", "Activity_Label")
+names(subject_test) = "subject"
 
-## step 5
-# find the mean for each combination of subject and label
-aggr.data <- aggregate(data.mean.std[, 3:ncol(data.mean.std)],
-                       by=list(subject = data.mean.std$subject, 
-                               label = data.mean.std$label),
-                       mean)
+# Bind data
+test_data <- cbind(as.data.table(subject_test), y_test, X_test)
 
-## step nothing
-# write the data for course upload
-write.table(format(aggr.data, scientific=T), "tidy2.txt",
-            row.names=F, col.names=F, quote=2)
+# Load and process X_train & y_train data.
+X_train <- read.table("train/X_train.txt")
+y_train <- read.table("train/y_train.txt")
+
+subject_train <- read.table("train/subject_train.txt")
+
+names(X_train) = features
+
+# Extract only the measurements on the mean and standard deviation for each measurement.
+X_train = X_train[,extract_features]
+
+# Load activity data
+y_train[,2] = activity_labels[y_train[,1]]
+names(y_train) = c("Activity_ID", "Activity_Label")
+names(subject_train) = "subject"
+
+# Bind data
+train_data <- cbind(as.data.table(subject_train), y_train, X_train)
+
+# Merge test and train data
+data = rbind(test_data, train_data)
+
+id_labels   = c("subject", "Activity_ID", "Activity_Label")
+data_labels = setdiff(colnames(data), id_labels)
+melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
+
+# Apply mean function to dataset using dcast function
+tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean)
+
+write.table(tidy_data, file = "./tidy_data.txt")
+
+
